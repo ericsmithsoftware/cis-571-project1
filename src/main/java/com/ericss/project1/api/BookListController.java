c7d070d9ea7867dba58ab539cdd3b0d36bf8b3f5
Eric Smith <seric16@gmail.com>
2020-02-17 01:03:16 -0500

    Adding json endpoint

--- src/main/java/com/ericss/project1/api/BookListController.java
+++ src/main/java/com/ericss/project1/api/BookListController.java
@@ -5,6 +5,8 @@ import com.ericss.project1.model.rest.Book;
 import com.ericss.project1.service.BookListService;
 import com.ericss.project1.validator.InputValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,20 @@ public class BookListController {
     @Autowired
     public BookListController(BookListService bookListService){
         this.bookListService = bookListService;
+    }
+
+    @GetMapping("/books/list.json")
+    public ResponseEntity<List<Book>> getBookListAsJson(
+            @RequestParam(name="numBooks", required=true) Integer numBooks,
+            @RequestParam(name="listCategories", required=true) List<String> bestSellerCategories) {
+        InputValidator.validateInput(numBooks, bestSellerCategories);
+        List<Book> bookList;
+        try{
+            bookList = bookListService.getBookList(numBooks, bestSellerCategories);
+        } catch(Exception e){
+            throw new InternalErrorException(e.getMessage());
+        }
+        return new ResponseEntity<List<Book>>(bookList, HttpStatus.OK);
     }
 
     @GetMapping("/books/list")
